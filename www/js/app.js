dec00fd0cde600b8f5653afa10634ba4369a3dca
lifenbo <lifenbo@lightinthebox.com>
2016-07-05 11:38:02 +0800

    add icon and splash

--- www/js/app.js
+++ www/js/app.js
@@ -91,6 +91,9 @@ angular.module('starter',
 		$ionicPlatform.ready(function() {
 			// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
 			// for form inputs)
+
+			// navigator.splashscreen.hide();
+
 			if (window.cordova && window.cordova.plugins
 					&& window.cordova.plugins.Keyboard) {
 				cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
