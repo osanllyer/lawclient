a5bcb6483528e0236dd7b9e9ad2efe69ff8aaed8
lifenbo <lifenbo@lightinthebox.com>
2016-11-12 22:27:56 +0800

    add real exam 2016

--- www/realexam/controller-real.js
+++ www/realexam/controller-real.js
@@ -1,7 +1,7 @@
 angular.module('starter.controllers')
 .controller('RealCtrl', function($scope, $state){
 	//真题tab页面
-	$scope.years = new Array('2015','2014','2013','2012','2011','2010','2009','2008','2007','2006','2005','2004','2003', '2002');
+	$scope.years = new Array('2016', '2015','2014','2013','2012','2011','2010','2009','2008','2007','2006','2005','2004','2003', '2002');
 	$scope.realPractice = function(year, paperId){
 		$state.go('tab.menu.exam', {year:year, paper:paperId});
 	};
